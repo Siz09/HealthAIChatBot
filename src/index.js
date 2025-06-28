5c7635518702986d657028b08701a0552657d6c6
Sijan Maharjan <112923124+Siz09@users.noreply.github.com>
2025-06-28 14:58:03 +0545

    resolved app.js issue

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.js";
 import { signInAnon, auth, onAuthStateChanged } from "./firebase-config";
 
 // Create the root once, outside of any callbacks
